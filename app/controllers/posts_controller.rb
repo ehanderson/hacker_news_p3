c04d59917bba43f9c0e026f4ed06d9f41af6bbfb
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-28 16:18:44 -0500

    WIP: added editability

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,13 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @post = Post.new
   end
 
   def create
     @post = Post.new(title: params[:post][:title], link: params[:post][:link], user_id: session[:user_id] )
-    p params
-    if @post.save!
+
+    if @post.save
       # session[:post_id] = @post.id
       redirect_to root_path
     else 
@@ -18,6 +19,16 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
+    redirect_to root_path
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(params[:post])
     redirect_to root_path
   end
 end
