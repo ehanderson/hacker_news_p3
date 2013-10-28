c866b80d1e5e9cccf0399a3f4392cef0d48adf20
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-28 09:28:16 -0500

    trying to delete the desktop files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackerNewsP3::Application.config.secret_token = '1d6f711b0ff6a9fb092ea0ede42c9fe164f512ad28350a5b794d9b7226396569c0d5f4d9e514c918fe9e9f58e5e65d3452525cae3eb445f128d0e3f10964e5bd'
