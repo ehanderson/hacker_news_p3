34d6ba8437840774f96e3044d95bbfa6bc2612e9
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-28 19:36:17 -0500

    final git commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,11 +6,32 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    puts "---------------------------------------------------------"
+    # puts "---------------------------------------------------------"
     puts params
     Comment.create(params[:comment]).update_attributes(post_id: params[:post_id], user_id: current_user.id)
 
-    # redirect_to post_comments_path(@post)
-    render "index"
+    redirect_to post_comments_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    Comment.find(params[:id]).destroy
+    redirect_to post_comments_path(@post)
+  end
+
+  def show
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    # puts "---------------------------------------------------------"
+    # puts @comment.inspect
+  end
+
+  def update
+    puts "---------------------------------------------------------"
+    puts params
+    @comment = Comment.find(params[:id])
+    @post = Post.find(params[:post_id])
+    @comment.update_attributes(params[:comment])
+    redirect_to post_comments_path
   end
 end
